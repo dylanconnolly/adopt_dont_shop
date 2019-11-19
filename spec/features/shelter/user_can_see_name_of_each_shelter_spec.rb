3c9168e10b3df1441218918be4eca5e76028753e
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-19 15:50:09 -0700

    Add additional details to shelter creation in test

--- spec/features/shelter/user_can_see_name_of_each_shelter_spec.rb
+++ spec/features/shelter/user_can_see_name_of_each_shelter_spec.rb
@@ -2,8 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "shelter index page", type: :feature do
   it "can see all shelter names" do
-    shelter_1 = Shelter.create(name: "Blue Blue Barky")
-    shelter_2 = Shelter.create(name: "The Puppy Pound")
+    shelter_1 = Shelter.create(name: "Blue Blue Barky",
+                               address: "123 This Way",
+                               city: "Denver",
+                               state: "CO",
+                               zip: "90204")
+
+    shelter_2 = Shelter.create(name: "Puppy Pound",
+                               address: "5608 N Eaton St.",
+                               city: "Chicago",
+                               state: "IL",
+                               zip: "80234")
 
     visit "/shelters"
 
