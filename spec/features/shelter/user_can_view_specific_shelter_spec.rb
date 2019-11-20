0ce1315ddc7e20bb2654a102d0c61721ad3af39f
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-19 17:30:40 -0700

    Create shelter show and write test

---
+++ spec/features/shelter/user_can_view_specific_shelter_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "shelter index page", type: :feature do
+  it "can see all shelter names" do
+    shelter_1 = Shelter.create(name: "Blue Blue Barky",
+                               address: "123 This Way",
+                               city: "Denver",
+                               state: "CO",
+                               zip: "90204")
+
+    shelter_2 = Shelter.create(name: "Puppy Pound",
+                               address: "5608 N Eaton St.",
+                               city: "Chicago",
+                               state: "IL",
+                               zip: "80234")
+
+    visit "/shelters/#{shelter_1.id}"
+
+    expect(page).to have_content(shelter_1.name)
+    expect(page).to have_content(shelter_1.address)
+    expect(page).to have_content(shelter_1.city)
+    expect(page).to have_content(shelter_1.state)
+    expect(page).to have_content(shelter_1.zip)
+
+    visit "/shelters/#{shelter_2.id}"
+
+    expect(page).to have_content(shelter_2.name)
+    expect(page).to have_content(shelter_2.address)
+    expect(page).to have_content(shelter_2.city)
+    expect(page).to have_content(shelter_2.state)
+    expect(page).to have_content(shelter_2.zip)
+  end
+end
