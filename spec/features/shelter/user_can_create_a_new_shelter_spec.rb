741f9dff15a4a6b83751ca39fde0de39fdce0e95
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-19 18:24:44 -0700

    Add test for form properly generating

--- spec/features/shelter/user_can_create_a_new_shelter_spec.rb
+++ spec/features/shelter/user_can_create_a_new_shelter_spec.rb
@@ -9,3 +9,16 @@ RSpec.describe "new shelter link", type: :feature do
     expect(page).to have_current_path("/shelters/new")
   end
 end
+
+RSpec.describe "new shelter form", type: :feature do
+  it "will ask for all shelter attributes" do
+
+    visit '/shelters/new'
+
+    expect(page).to have_field("shelter[name]")
+    expect(page).to have_field("shelter[address]")
+    expect(page).to have_field("shelter[city]")
+    expect(page).to have_field("shelter[state]")
+    expect(page).to have_field("shelter[zip]")
+  end
+end
