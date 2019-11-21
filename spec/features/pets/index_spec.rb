39db2df2c8ca393fac3723e401a5da220b464696
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-21 14:53:37 -0700

    Create pet model and migrate tables

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "pets index", type: :feature do
   it "lists all pets with their details" do
-      pet_1 = Pet.create(name: "Rufus")
-      pet_2 = Pet.create(name: "Peter")
+      pet_1 = Pet.create(name: "Rufus", image: "some_path", approximate_age: "4", sex: "male")
+      pet_2 = Pet.create(name: "Peter", image: "some_url", approximate_age: "1", sex: "male")
 
     visit '/pets'
 
