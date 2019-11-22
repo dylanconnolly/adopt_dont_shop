7d4dc8c48e7abeb5997ec3efd6d7e76f1b03f6d1
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-22 11:17:37 -0700

    Add destory spec file with intial test for existence of delete button on show page

---
+++ spec/features/pets/destroy_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "pet can be deleted" do
+  before :each do
+    @shelter_1 = Shelter.create(name: "Blue Blue Barky",
+                               address: "123 This Way",
+                               city: "Denver",
+                               state: "CO",
+                               zip: "90204")
+
+    @pet_1 = Pet.create(name: "Rufus", image: "https://cdn.pixabay.com/photo/2018/05/07/10/48/husky-3380548_1280.jpg", approximate_age: "4", sex: "male", shelter: @shelter_1)
+  end
+
+  it "there is a delete button on pet show page" do
+
+    visit "/pets/#{@pet_1.id}"
+
+    click_link "Delete Pet"
+  end
+
+end
