7a2fecbbfd79506a9a50fec51da0a1521a20bd5e
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-22 10:37:40 -0700

    Add route to point to /pets/id/edit

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   get '/pets/:id', to: 'pets#show'
   get '/shelters/:id/pets/new', to: 'pets#new'
   post '/shelters/:id/pets', to: 'pets#create'
+  get '/pets/:pet_id/edit', to: 'pets#edit'
 end
