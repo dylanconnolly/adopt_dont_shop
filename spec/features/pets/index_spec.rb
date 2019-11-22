848d4e6ab0af7d4dbfbbbf167bef2695c3ba43d3
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-21 20:52:01 -0700

    Change image attribute to include photos for pets

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -14,18 +14,18 @@ RSpec.describe "pets index", type: :feature do
                               state: "IL",
                               zip: "80234")
 
-    pet_1 = Pet.create(name: "Rufus", image: "some_path", approximate_age: "4", sex: "male", shelter: shelter_1)
-    pet_2 = Pet.create(name: "Peter", image: "some_url", approximate_age: "1", sex: "male", shelter: shelter_2)
+    pet_1 = Pet.create(name: "Rufus", image: "https://cdn.pixabay.com/photo/2018/05/07/10/48/husky-3380548_1280.jpg", approximate_age: "4", sex: "male", shelter: shelter_1)
+    pet_2 = Pet.create(name: "Peter", image: "https://cdn.pixabay.com/photo/2016/05/09/10/42/weimaraner-1381186_1280.jpg", approximate_age: "1", sex: "male", shelter: shelter_2)
 
     visit '/pets'
 
-    find("img[src='some_path']")
+    find("img[src='https://cdn.pixabay.com/photo/2018/05/07/10/48/husky-3380548_1280.jpg']")
     expect(page).to have_content(pet_1.name)
     expect(page).to have_content(pet_1.sex)
     expect(page).to have_content(pet_1.approximate_age)
     expect(page).to have_content(shelter_1.name)
 
-    find("img[src='some_url']")
+    find("img[src='https://cdn.pixabay.com/photo/2016/05/09/10/42/weimaraner-1381186_1280.jpg']")
     expect(page).to have_content(pet_2.name)
     expect(page).to have_content(pet_2.sex)
     expect(page).to have_content(pet_2.approximate_age)
--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -14,12 +14,12 @@ RSpec.describe "show pets", type: :feature do
                               state: "IL",
                               zip: "80234")
 
-    pet_1 = Pet.create(name: "Rufus", image: "some_path", approximate_age: "4", sex: "male", shelter: shelter_1)
-    pet_2 = Pet.create(name: "Peter", image: "some_url", approximate_age: "1", sex: "male", shelter: shelter_2)
+    pet_1 = Pet.create(name: "Rufus", image: "https://cdn.pixabay.com/photo/2018/05/07/10/48/husky-3380548_1280.jpg", approximate_age: "4", sex: "male", shelter: shelter_1)
+    pet_2 = Pet.create(name: "Peter", image: "https://cdn.pixabay.com/photo/2016/05/09/10/42/weimaraner-1381186_1280.jpg", approximate_age: "1", sex: "male", shelter: shelter_2)
 
     visit "/pets/#{pet_1.id}"
 
-    find("img[src='some_path']")
+    find("img[src='https://cdn.pixabay.com/photo/2018/05/07/10/48/husky-3380548_1280.jpg']")
     expect(page).to have_content(pet_1.name)
     expect(page).to have_content(pet_1.description)
     expect(page).to have_content(pet_1.approximate_age)
@@ -29,7 +29,7 @@ RSpec.describe "show pets", type: :feature do
 
     visit "/pets/#{pet_2.id}"
 
-    find("img[src='some_url']")
+    find("img[src='https://cdn.pixabay.com/photo/2016/05/09/10/42/weimaraner-1381186_1280.jpg']")
     expect(page).to have_content(pet_2.name)
     expect(page).to have_content(pet_2.description)
     expect(page).to have_content(pet_2.approximate_age)
