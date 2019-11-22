942ccf527379e0ffaf0ff92c39b66aef63eca1a4
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-21 20:49:54 -0700

    Add test for testing adopted pets do not load on shelter page

--- spec/features/shelter/show_pets_spec.rb
+++ spec/features/shelter/show_pets_spec.rb
@@ -1,29 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe "shelter pet index", type: :feature do
-  it "lists all the shelter's pets and their details" do
+  before :each do
 
-    shelter_1 = Shelter.create(name: "Blue Blue Barky",
-                               address: "123 This Way",
-                               city: "Denver",
-                               state: "CO",
-                               zip: "90204")
+    @shelter_1 = Shelter.create(name: "Blue Blue Barky",
+      address: "123 This Way",
+      city: "Denver",
+      state: "CO",
+      zip: "90204")
 
-    shelter_2 = Shelter.create(name: "Rodeo Roundup Soundup Pupps",
-                              address: "5608 N Eaton St.",
-                              city: "Chicago",
-                              state: "IL",
-                              zip: "80234")
+    @shelter_2 = Shelter.create(name: "Rodeo Roundup Soundup Pupps",
+      address: "5608 N Eaton St.",
+      city: "Chicago",
+      state: "IL",
+      zip: "80234")
 
-    pet_1 = Pet.create(name: "Rufus", image: "some_path", approximate_age: "4", sex: "male", shelter: shelter_1)
-    pet_2 = Pet.create(name: "Peter", image: "some_url", approximate_age: "1", sex: "male", shelter: shelter_2)
-
-    visit "/shelters/#{shelter_1.id}/pets"
-
-    find("img[src='some_path']")
-    expect(page).to have_content(pet_1.name)
-    expect(page).to have_content(pet_1.approximate_age)
-    expect(page).to have_content(pet_1.sex)
-    expect(page).to_not have_content(pet_2.name)
+    @pet_1 = Pet.create(name: "Rufus", image: "https://cdn.pixabay.com/photo/2018/05/07/10/48/husky-3380548_1280.jpg", approximate_age: "4", sex: "male", shelter: @shelter_1)
+    @pet_2 = Pet.create(name: "Peter", image: "https://cdn.pixabay.com/photo/2016/05/09/10/42/weimaraner-1381186_1280.jpg", approximate_age: "1", sex: "male", shelter: @shelter_2)
+    @pet_3 = Pet.create(name: "Bella", image: "https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_1280.jpg", approximate_age: "3", sex: "female", shelter: @shelter_1, adoptable: false)
   end
+
+  it "lists the shelter's adoptable pets and their details" do
+
+    visit "/shelters/#{@shelter_1.id}/pets"
+
+    find("img[src='https://cdn.pixabay.com/photo/2018/05/07/10/48/husky-3380548_1280.jpg']")
+    expect(page).to have_content(@pet_1.name)
+    expect(page).to have_content(@pet_1.approximate_age)
+    expect(page).to have_content(@pet_1.sex)
+
+    visit "/shelters/#{@shelter_2.id}/pets"
+
+    expect(page).to have_content(@pet_2.name)
+    expect(page).to have_content(@pet_2.approximate_age)
+  end
+
+  it "does not display pets that are pending adoption" do
+
+    visit "/shelters/#{@shelter_1.id}/pets"
+
+    expect(page).to_not have_content(@pet_3.name)
+    expect(page).to_not have_content(@pet_3.sex)
+  end
+
 end
