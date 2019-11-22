6b1b282836f94cfee05a4ad1221af4c5b4fced45
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-22 11:42:53 -0700

    Add test to shelters index spec to check for edit links for each shelter

--- spec/features/shelter/index_spec.rb
+++ spec/features/shelter/index_spec.rb
@@ -1,23 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "shelter index page", type: :feature do
-  it "can see all shelter names" do
-    shelter_1 = Shelter.create(name: "Blue Blue Barky",
-                               address: "123 This Way",
-                               city: "Denver",
-                               state: "CO",
-                               zip: "90204")
+  before :each do
+    @shelter_1 = Shelter.create(name: "Blue Blue Barky",
+      address: "123 This Way",
+      city: "Denver",
+      state: "CO",
+      zip: "90204")
 
-    shelter_2 = Shelter.create(name: "Puppy Pound",
-                               address: "5608 N Eaton St.",
-                               city: "Chicago",
-                               state: "IL",
-                               zip: "80234")
+    @shelter_2 = Shelter.create(name: "Puppy Pound",
+      address: "5608 N Eaton St.",
+      city: "Chicago",
+      state: "IL",
+      zip: "80234")
+  end
+
+  it "can see all shelter names" do
 
     visit "/shelters"
 
-    expect(page).to have_content(shelter_1.name)
+    expect(page).to have_content(@shelter_1.name)
 
-    expect(page).to have_content(shelter_2.name)
+    expect(page).to have_content(@shelter_2.name)
+  end
+
+  it "has an edit link for each shelter and link takes user to that shelter's edit form" do
+
+    visit "/shelters"
+
+    expect(page).to have_link("/shelters/#{@shelter_1.id}/edit")
+    expect(page).to have_link("/shelters/#{@shelter_2.id}/edit")
+
   end
 end
