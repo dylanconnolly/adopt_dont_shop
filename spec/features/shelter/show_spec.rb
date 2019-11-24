20b77dd2d613050ef839b564d032fa125c44653a
Dylan Connolly <dylanconnolly145@gmail.com>
2019-11-24 15:59:37 -0700

    Add test to shelter show spec to test for link to view shelter pets index

--- spec/features/shelter/show_spec.rb
+++ spec/features/shelter/show_spec.rb
@@ -30,4 +30,25 @@ RSpec.describe "individual shelter page", type: :feature do
     expect(page).to have_content(shelter_2.state)
     expect(page).to have_content(shelter_2.zip)
   end
+
+  it "has a link to view pets of that shelter" do
+
+    shelter_1 = Shelter.create(name: "Blue Blue Barky",
+                               address: "123 This Way",
+                               city: "Denver",
+                               state: "CO",
+                               zip: "90204")
+
+    shelter_2 = Shelter.create(name: "Puppy Pound",
+                               address: "5608 N Eaton St.",
+                               city: "Chicago",
+                               state: "IL",
+                               zip: "80234")
+
+    visit "/shelters/#{shelter_1.id}"
+
+    click_link("View Pets")
+
+    expect(current_path).to eq("/shelters/#{shelter_1.id}/pets")
+  end
 end
